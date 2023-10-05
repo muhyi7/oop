490c7891d2c3d948c45c2a5e4088a10c8ab4c27f
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2023-10-05 07:37:10 +0700

    Finish

--- home.php
+++ home.php
@@ -1,40 +1,76 @@
 <?php
 require('koneksi.php');
 require('query.php');
-$email = $_GET['user_fullname'];
+$email = $_GET['user_fullname'] ?? false;
 $obj = new crud;
 ?>
-<html>
+
+<!DOCTYPE html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <!-- Tambahkan link ke file CSS Bootstrap di sini -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
-    <h1>Selamat Datang<?php echo $email; ?></h1>
-    <table border="1">
-        <tr>
-            <td>No</td>
-            <td>Email</td>
-            <td>Nama</td>
-            <td></td>
-        </tr>
-        <?php
-        $data = $obj->lihatData();
-        $no = 1;
-        if ($data->rowCount() > 0) {
-            while ($row = $data->fetch(PDO::FETCH_ASSOC)) { ?>
-                <tr>
-                    <td><?php echo $no; ?></td>
-                    <td><?php echo $row['user_email']; ?></td>
-                    <td><?php echo $row['user_fullname']; ?></td>
-                    <td>
-                        <a href="edit.php?id=<?php echo $row['id']; ?>" edit</a>
-                            <a href="hapus.php?id=<?php echo $row['id']; ?>" hapus</a>
-                    </td>
-                </tr>
-        <?php
-                $no++;
-            }
-        } ?>
-    </table>
+<div id="wrapper">
+        <!-- Tambahkan sidebar sesuai dengan template SB Admin -->
+        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+            <!-- Tambahkan konten sidebar di sini -->
+        </ul>
+
+        <div id="content-wrapper" class="d-flex flex-column">
+            <div id="content">
+                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+                    <!-- Tambahkan konten navbar di sini -->
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="login.php">Logout</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="register.php">Register</a>
+                        </li>
+                    </ul>
+                </nav>
+                <div class="container-fluid">
+                    <h1 class="h3 mb-4 text-gray-800">Selamat Datang <?php echo $email; ?></h1>
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Email</th>
+                                <th>Nama</th>
+                                <th>Action</th> <!-- Kolom untuk tombol Edit dan Hapus -->
+                            </tr>
+                        </thead>
+                        <tbody>
+                <?php
+                $data = $obj->lihatData();
+                $no = 1;
+                if ($data->rowCount() > 0) {
+                    while ($row = $data->fetch(PDO::FETCH_ASSOC)) { ?>
+                        <tr>
+                            <td><?php echo $no; ?></td>
+                            <td><?php echo $row['user_email']; ?></td>
+                            <td><?php echo $row['user_fullname']; ?></td>
+                            <td>
+                                <a href="edit.php?id=<?php echo $row['id']; ?>" class="btn btn-primary">Edit</a>
+                                <a href="hapus.php?id=<?php echo $row['id']; ?>" class="btn btn-danger">Hapus</a>
+                            </td>
+                        </tr>
+                <?php
+                        $no++;
+                    }
+                } ?>
+            </tbody>
+        </table>
+    </div>
+
+    <!-- Tambahkan script Bootstrap di sini (JQuery, Popper.js, Bootstrap JS) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
--- register.php
+++ register.php
@@ -1,18 +1,74 @@
 <?php
-require ('koneksi.php');
-require ('query.php');
+require('koneksi.php');
+require('query.php');
 
-$obj = new crud;
+$obj = new crud();
 
-if($_SERVER['REQUEST_METHOD']=='POST');
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = $_POST['txt_email'];
     $pass = $_POST['txt_pass'];
     $name = $_POST['txt_name'];
-    if($obj->insertData($email, $pass, $name));
-        echo '<div class="alert alert-succes">Data berhasil disimpan</div>';
-else:
-    echo '<div class="alert alert-danger">Data gagal disimpan</div>';
-    endif;
-endif;
+
+    if ($obj->insertData($email, $pass, $name)) {
+        $message = '<div class="alert alert-success alert-dismissible fade show" role="alert">
+                    Data berhasil disimpan
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>';
+    } else {
+        $message = '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    Data gagal disimpan
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>';
+    }
+}
 ?>
-<html
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Register</title>
+    <!-- Tambahkan link ke file CSS Bootstrap di sini -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+
+<body class="bg-light">
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header bg-primary text-white">Register</div>
+                    <div class="card-body">
+                        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+                            <div class="form-group">
+                                <label for="txt_email">Email:</label>
+                                <input type="text" class="form-control" id="txt_email" name="txt_email" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="txt_pass">Password:</label>
+                                <input type="password" class="form-control" id="txt_pass" name="txt_pass" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="txt_name">Nama:</label>
+                                <input type="text" class="form-control" id="txt_name" name="txt_name" required>
+                            </div>
+                            <button type="submit" name="register" class="btn btn-primary">Register</button>
+                        </form>
+                    </div>
+                </div>
+                <?php echo isset($message) ? $message : ''; ?>
+            </div>
+        </div>
+    </div>
+
+    <!-- Tambahkan script Bootstrap di sini (JQuery, Popper.js, Bootstrap JS) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+
+</html>
