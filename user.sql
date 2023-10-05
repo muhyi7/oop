15e150fae031287193e0ad50b6cbee737e6bd094
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2023-10-05 08:00:04 +0700

    add sql database

---
+++ user.sql
@@ -0,0 +1,111 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 05 Okt 2023 pada 02.57
+-- Versi server: 10.4.24-MariaDB
+-- Versi PHP: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `user`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `level_detail`
+--
+
+CREATE TABLE `level_detail` (
+  `id_level` tinyint(4) NOT NULL,
+  `level` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `level_detail`
+--
+
+INSERT INTO `level_detail` (`id_level`, `level`) VALUES
+(1, 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user_detail`
+--
+
+CREATE TABLE `user_detail` (
+  `id` int(11) NOT NULL,
+  `user_email` varchar(50) NOT NULL,
+  `user_password` varchar(50) NOT NULL,
+  `user_fullname` varchar(100) NOT NULL,
+  `level` tinyint(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `user_detail`
+--
+
+INSERT INTO `user_detail` (`id`, `user_email`, `user_password`, `user_fullname`, `level`) VALUES
+(1, 'user1@user.com', '12344321', 'userAku', 2),
+(2, 'admin@admin.com', '12344321', 'administrator', 1),
+(3, 'muhyi@gmail.com', 'admin123', 'Muhyi', 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `level_detail`
+--
+ALTER TABLE `level_detail`
+  ADD PRIMARY KEY (`id_level`);
+
+--
+-- Indeks untuk tabel `user_detail`
+--
+ALTER TABLE `user_detail`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `level` (`level`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `level_detail`
+--
+ALTER TABLE `level_detail`
+  MODIFY `id_level` tinyint(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `user_detail`
+--
+ALTER TABLE `user_detail`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `level_detail`
+--
+ALTER TABLE `level_detail`
+  ADD CONSTRAINT `level_detail_ibfk_1` FOREIGN KEY (`id_level`) REFERENCES `user_detail` (`level`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
